181427fbcc58483659dcf5f7a8f1dd6f540454f7
ZenPtc <maku4fb@gmail.com>
2021-01-12 13:56:57 +0700

    1part

--- app_template.js
+++ app_template.js
@@ -1,1 +1,26 @@
 //your code here!
+document.addEventListener('DOMContentLoaded',function(){
+    let sum=620610794
+    document.querySelector('#button-compute').onclick = function(){
+        const par = document.querySelector('#stdNum');
+        const number = document.querySelector("#input-number").value;
+
+        sum += +number;
+        par.innerHTML = sum;
+    }
+
+    document.querySelector('#button-create-table').onclick = function(){
+        const row = document.querySelector("#input-row").value;
+        const col = document.querySelector("#input-col").value;
+
+        let num=0;
+
+        for(let i=0;i<row;i++){
+            const tr = document.createElement('tr');
+            for(let j=0;j<col;j++){
+                const td = document.createElement('td');
+                td.innerHTML = ++num;
+            }
+        }
+    }
+})
